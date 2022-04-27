c5d7a60cbc6a545abe18d893c109cf90d8d372aa
Rezon Jay Tabigue <rezonjaytabigue@Rezon-Jay-Tabigue-MBP16.local>
2022-04-27 11:51:21 +0800

    php-401

---
+++ php-401/index.php
@@ -0,0 +1,9 @@
+<?php
+    $str = "<p>In Italy<i>pasta</i> is commonly <br />
+    eaten during <b>Lunch and Dinner</b> to have a lighter stomach.<br />
+    The only thing that is common for Italians is that <i>coffee</i> is always served 3 or 4 times a day.</p>
+    <script>alert(\"Cafe!\");</script>
+    ";
+
+    echo strip_tags($str,'<p><i>');
+?>
